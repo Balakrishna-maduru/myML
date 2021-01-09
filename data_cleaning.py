1f8a7cb02381692b3a14976084a12ffb616a65d3
Balakrishna <maduru.balakrishna@nokia.com>
2021-01-10 04:45:19 +0530

    project structure creation

---
+++ data_cleaning.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+
+if __name__ == '__main__':
+	
+	survey_key =  "MSISDN"
+	file_name = r"E:\my_work\data\nei_data\2020\CEI_O_INDEX_DAY_11_perc_kpi.csv"
---
+++ test/test_read_data/read_data_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class ReadDataTest(unittest.TestCase):
+    def test_get_files_to_read(self):
+
+
+
+if __name__ == '__main__':
+    
