4b3781cbdf249b5d0c875ee87457cf6c5ff5e4df
Balakrishna <maduru.balakrishna@nokia.com>
2021-01-10 05:24:35 +0530

    project structure creation

--- test/test_read_data/read_data_test.py
+++ test/test_read_data/read_data_test.py
@@ -1,9 +1,20 @@
 import unittest
 
-class ReadDataTest(unittest.TestCase):
-    def test_get_files_to_read(self):
+class ReadData:
 
+    def _get_files_to_read(self):
+        return [1,2,3]
+
+
+class ReadDataTest(unittest.TestCase):
+
+
+    def test_get_files_to_read(self):
+        print("Test")
+        read_data = ReadData()
+        self.assertEqual(read_data._get_file_to_read(),[1,2,3])
 
 
 if __name__ == '__main__':
+    pass
     
