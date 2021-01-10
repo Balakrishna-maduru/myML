13ca4d8258be4ea7570ab3a900bff20567380a80
Balakrishna <maduru.balakrishna@nokia.com>
2021-01-10 06:37:24 +0530

    refactering project

--- pre_processing/read_data.py
+++ pre_processing/read_data.py
@@ -4,10 +4,10 @@ class ReadData:
 
     def _get_files_to_read(self, path):
         files_to_read = []
-        if os.isfile(path):
+        if path.isfile():
             return [path]
         else:
             for file in os.scandir(path):
-                if file.is_file():
+                if file.isfile():
                     files_to_read.append(file)
             return files_to_read
--- test/test_read_data/read_data_test.py
+++ test/test_read_data/read_data_test.py
@@ -1,14 +1,18 @@
+import sys
+sys.path.append('C:/Users/mabalakr/source/repos/myML/')
 import unittest
+
 from pre_processing.read_data import ReadData
 
 
 class ReadDataTest(unittest.TestCase):
 
+    def setUp(self):
+        self.widget = Widget('The widget')
 
     def test_get_files_to_read_file_path(self):
-        print("Test")
         read_data = ReadData()
-        self.assertEqual(read_data._get_files_to_read('data/ReadData/read_data.csv'),[1,2,3])
+        self.assertEqual(read_data._get_files_to_read('/data/ReadData/read_data.csv'),[1,2,3])
 
 
 if __name__ == '__main__':
