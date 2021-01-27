50769987aacdef5ab2107830cef4416eca8d8950
Balakrishna <maduru.balakrishna@nokia.com>
2021-01-27 10:00:53 +0530

    add scaler changes

--- pre_processing/scaler.py
+++ pre_processing/scaler.py
@@ -2,13 +2,17 @@ import pandas as pd
 from sklearn import preprocessing
 class Scaler:
 
-    def __init__(self, type_of_scaler = "min_max_scaler"):
+    def __init__(self, type_of_scaler = "min_max_scaler", quantile_range = (0.1,0.9)):
         if type_of_scaler == "min_max_scaler":
             self.scaler = preprocessing.MinMaxScaler()
         elif type_of_scaler == "standard_scaler":
             self.scaler = preprocessing.StandardScaler()
         elif type_of_scaler == "max_abs_scaler":
             self.scaler = preprocessing.MaxAbsScaler()
+        elif type_of_scaler == "robust_scaler":
+            self.scaler = preprocessing.RobustScaler()
+        elif type_of_scaler == "robust_scaler":
+            self.scaler = preprocessing.RobustScaler()
 
     def scale(self, df, key):
         key_id = df[key]
